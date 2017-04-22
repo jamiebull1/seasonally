7766a50bc77f5e0cb549b4bf42b2b28217f3ad17
Jamie Bull <jamie.bull@oco-carbon.com>
2017-04-22 19:45:35 +0200

    Create the suggest app

---
+++ suggest/views.py
@@ -0,0 +1,41 @@
+"""Views for the seasonal suggestions website."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import datetime
+
+from django.shortcuts import render
+
+from api.models import Product
+
+
+def index(request):
+    """Homepage."""
+    today = datetime.datetime.now()
+    abbr_month = today.strftime('%b').lower()
+    month = today.strftime('%B')
+    context = {'abbr_month': abbr_month, 'month': month}
+    month_num = int(today.strftime('%m'))
+    context['recipes'] = []
+    # fetch items for this month
+    while len(context['recipes']) < 3:
+        # choose a product
+        product = fetch_product(month_num)
+        # fetch a recipe using the product
+        recipe = fetch_recipe(product)
+        if recipe not in context['recipes']:
+            context['recipes'].append(recipe)
+    return render(request, 'suggest/index.html', context=context)
+
+
+def fetch_product(month_num):
+    """Fetch a random seasonal product from the database."""
+    product = Product.objects.filter(months__num=month_num).order_by('?')[0]
+    return product
+
+
+def fetch_recipe(product):
+    """Fetch a random recipe from the chosen product."""
+    return product.recipe.values().order_by('?')[0]
