98c54456b6f505a56a0bb303627d3809982ee218
jamie.bull <jamie.bull@kiwi.com>
2018-01-10 11:31:30 +0100

    Use products from `common`

--- scraper/scraper/spiders/allrecipescouk.py
+++ scraper/scraper/spiders/allrecipescouk.py
@@ -14,6 +14,7 @@ from six import text_type
 from six.moves import urllib
 from six.moves.urllib.parse import urljoin
 
+from . import common
 from ..items import RecipeItem
 
 
@@ -21,20 +22,7 @@ class AllrecipesukSpider(scrapy.Spider):
     name = "allrecipescouk"
     allowed_domains = ["allrecipes.co.uk"]
     root = "http://allrecipes.co.uk"
-    products = [
-        "Apple", "Apricot", "Asparagus", "Aubergine", "Banana", "Basil", "Beef", "Beetroot", "Blackberry",
-        "Blackcurrants", "Bramley apple", "Broad bean", "Broad beans", "Broccoli", "Brussels sprouts", "Cabbage",
-        "Carrot", "Cauliflower", "Cavolo nero", "Celeriac", "Celery", "Cherry", "Chervil", "Chestnut", "Chicken",
-        "Chicory", "Clementine", "Cod", "Courgette", "Courgette flower", "Crab", "Cranberry", "Damson", "Date", "Duck",
-        "Fennel bulb", "Fig", "Garlic", "Globe artichoke", "Goose", "Gooseberry", "Grapefruit", "Grouse", "Guinea fowl",
-        "Halibut", "Jerusalem artichoke", "Kale", "Kipper", "Kohlrabi", "Lamb", "Lamb's lettuce", "Leek", "Lemon",
-        "Lettuce", "Mackerel", "Marrow", "Mint", "Mussels", "Nectarine", "New potatoes", "Onion", "Orange", "Oyster",
-        "Pak choi", "Parsnip", "Peach", "Pear", "Peas", "Pepper", "Plum", "Pomegranate", "Pork", "Potato", "Pumpkin",
-        "Purple sprouting broccoli", "Quince", "Radicchio", "Radish", "Raspberry", "Redcurrant", "Rhubarb",
-        "Runner bean", "Salmon", "Salsify", "Samphire", "Sorrel", "Spinach", "Spring greens", "Spring lamb",
-        "Spring onion", "Strawberry", "Swede", "Sweet potato", "Sweetcorn", "Swiss chard", "Tomato", "Tuna", "Turkey",
-        "Turnip", "Venison", "Watercress", "Watermelon", "Whiting",
-    ]
+    products = common.products
     # products = ['Apple']
     recipe_selector = '//a[starts-with(@href, "http://allrecipes.co.uk/recipe/")]/@href'
     def start_requests(self):
