0255b276b7ef0c2fab6e0098535227d86bc58f55
Jamie Bull <jamie.bull@kiwi.com>
2018-09-24 17:51:22 +0200

    whitenoise: fix backwards non-compatibility issue
    / The `whitenoise.django.GzipManifestStaticFilesStorage` alias has now
    been removed. Instead you should use the correct import path:
    `whitenoise.storage.CompressedManifestStaticFilesStorage`
    
    http://whitenoise.evans.io/en/stable/changelog.html#v4-0

--- seasonally/settings.py
+++ seasonally/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 S3_BUCKET = 'http://seasonally-assets.s3.us-east-2.amazonaws.com/'
 
