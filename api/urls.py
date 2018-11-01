5295c659eb38c3eb69473aa79e8d5990f3d9e185
jamiebull1 <jamiebull1@gmail.com>
2018-11-01 14:23:50 +0100

    REmove __future__imports as we're now running in py3.7

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,6 @@
 """seasonally API URL Configuration
 
 """
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 from django.conf.urls import url
 
 from api import views
--- seasonally/urls.py
+++ seasonally/urls.py
@@ -1,11 +1,6 @@
 """seasonally URL Configuration
 
 """
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
